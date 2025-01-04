49fece26364f0f87575adfa392f7dbf18e6db8d2
lydz <chouakilydia@gmail.com>
2025-01-04 10:31:16 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+st.write("Hello World")
+data= {"a":[1,2,3],"b":[4,5, 6]}
+df= pd.DataFrame(data)
+tab1, tab2, tab3 = st.tabs(["Cat","Dog","Owl"])
+with tab1:
+    st.header("A cat")
+    st.write(f"nos données: ")
+    st.write(duckdb.sql("select * from df").df())
+    query=st.text_area(label="Entrez votre requete ")
+    st.write(f"Vous avez rentré cette requete: {query}")
+    st.write(duckdb.sql(query).df())
+
+with tab2:
+    st.header("A dog")
+
+with tab3:
+    st.header("An owl")
